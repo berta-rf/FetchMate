5b9f6e81afa5637ca44924e3bdf0c0a1a5a65cdc
AlisonEve <alisoneve92@gmail.com>
2023-08-19 10:41:35 +0100

    Added test for no matches

--- src/test_utils.py
+++ src/test_utils.py
@@ -1,18 +1,28 @@
 from unittest import TestCase, main
+from unittest.mock import patch
+
 from src.utils import get_matched_breeds
 
 
 class TestMatchedBreeds(TestCase):
-    def test_get_matched_breeds(self):
-        quiz_inputs = {
-            "shedding": 4,
+
+    @patch('requests.get')
+    def test_no_matches(self, mock_get):
+        mock_get.return_value.status_code = 200
+        mock_get.return_value.json.return_value = []
+        quiz_input = {
+            "shedding": 1,
             "barking": 1,
-            "energy": 3,
-            "protectiveness": 3,
-            "trainability": 5
+            "energy": 1,
+            "protectiveness": 1,
+            "trainability": 1,
         }
-        self.assertEqual(get_matched_breeds(quiz_inputs), {"Golden Retriever": "static/img/Golden Retriever.jpg"})
+        result = get_matched_breeds(quiz_input, n=2)
+        expected = {
+            "You're a Cat Person🐱": "static/img/cat-white-wall.jpg"
+        }
+        self.assertEqual(result, expected)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
