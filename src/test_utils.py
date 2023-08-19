1418234380be67586126058070175d7ae8c1e00a
AlisonEve <alisoneve92@gmail.com>
2023-08-19 11:50:44 +0100

    Added mock test for perfect matches

--- src/test_utils.py
+++ src/test_utils.py
@@ -1,10 +1,30 @@
 from unittest import TestCase, main
 from unittest.mock import patch
-
 from src.utils import get_matched_breeds
 
 
 class TestMatchedBreeds(TestCase):
+
+    @patch('requests.get')
+    def test_perfect_matches(self, mock_get):
+        mock_get.return_value.status_code = 200
+        mock_get.return_value.json.return_value = [
+            {"name": "Affenpinscher"},
+            {"name": "Appenzeller Sennenhund"}
+        ]
+        quiz_input = {
+            "shedding": 3,
+            "barking": 1,
+            "energy": 3,
+            "protectiveness": 3,
+            "trainability": 5,
+        }
+        result = get_matched_breeds(quiz_input, n=2)
+        expected = {
+            "Affenpinscher": f"static/img/Affenpinscher.jpg",
+            "Appenzeller Sennenhund": f"static/img/Appenzeller Sennenhund.jpg"
+        }
+        self.assertEqual(result, expected)
 
     @patch('requests.get')
     def test_no_matches(self, mock_get):
@@ -26,3 +46,6 @@ class TestMatchedBreeds(TestCase):
 
 if __name__ == '__main__':
     main()
+
+
+
