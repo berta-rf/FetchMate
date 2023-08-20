66335e4ae35cc5b8b9ba21d424911ab09c914273
AlisonEve <alisoneve92@gmail.com>
2023-08-20 16:13:28 +0100

    Added another edge case, experimented with mock tests

--- src/test_utils.py
+++ src/test_utils.py
@@ -1,17 +1,18 @@
 from unittest import TestCase, main
-from src.utils import get_matched_breeds
+from unittest.mock import patch
+from src import utils
+from utils import get_matched_breeds, download_dog_image, requests
 
 
 class TestMatchedBreeds(TestCase):
 
-    def test_perfect_matches(self):
-        quiz_input = {
-            "shedding": 3,
-            "barking": 1,
-            "energy": 3,
-            "protectiveness": 3,
-            "trainability": 5,
-        }
+    @patch("utils.download_dog_image")
+    def test_perfect_matches(self, mock_download_dog_image):
+        quiz_input = dict(shedding=3, barking=1, energy=3, protectiveness=3, trainability=5)
+
+        mock_download_dog_image.return_value.status_code = 200
+        mock_download_dog_image.return_value = [dict(name="Affenpinscher"), dict(name="Appenzeller Sennenhund")]
+
         result = get_matched_breeds(quiz_input, n=2)
         expected = {
             "Affenpinscher": f"static/img/Affenpinscher.jpg",
@@ -19,13 +20,7 @@ class TestMatchedBreeds(TestCase):
         }
         self.assertEqual(result, expected)
 
-        quiz_input = {
-            "shedding": 1,
-            "barking": 1,
-            "energy": 1,
-            "protectiveness": 1,
-            "trainability": 1,
-        }
+        quiz_input = dict(shedding=1, barking=1, energy=1, protectiveness=1, trainability=1)
 
         result = get_matched_breeds(quiz_input, n=2)
         expected = {
@@ -34,15 +29,18 @@ class TestMatchedBreeds(TestCase):
         }
         self.assertEqual(result, expected)
 
+        quiz_input = dict(shedding=5, barking=5, energy=5, protectiveness=5, trainability=5)
+        result = get_matched_breeds(quiz_input, n=2)
+        expected = {
+            "Barbado da Terceira": "static/img/Barbado da Terceira.jpg",
+         "Doberman Pinscher": "static/img/Doberman Pinscher.jpg"
+        }
+        self.assertEqual(result, expected)
+
     def test_no_matches(self):
 
-        quiz_input = {
-            "shedding": 0,
-            "barking": 0,
-            "energy": 0,
-            "protectiveness": 0,
-            "trainability": 0,
-        }
+        quiz_input = dict(shedding=0, barking=0, energy=0, protectiveness=0, trainability=0)
+
         result = get_matched_breeds(quiz_input, n=2)
         expected = {
             "You're a Cat Person🐱": "static/img/cat-white-wall.jpg"
@@ -50,8 +48,10 @@ class TestMatchedBreeds(TestCase):
         self.assertEqual(result, expected)
 
 
-if __name__ == '__main__':
-    main()
+# need testing for download_dog_image
+
+# if __name__ == '__main__':
+#     main()
 
 
 
