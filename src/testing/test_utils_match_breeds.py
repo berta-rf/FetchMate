e6c4e833609985f1b3af2e0b91b11dca6c2a0e62
AlisonEve <alisoneve92@gmail.com>
2023-08-24 16:38:27 +0100

    Changed the directory path so that the function and the testing are producing the same paths

--- src/testing/test_utils_match_breeds.py
+++ src/testing/test_utils_match_breeds.py
@@ -1,5 +1,6 @@
 from unittest import TestCase, main
 from unittest.mock import patch
+import os
 from src.utils import get_matched_breeds
 
 
@@ -12,12 +13,20 @@ class TestMatchedBreeds(TestCase):
         mock_download_dog_image.return_value.status_code = 200  # returns a successful HTTP response
         mock_download_dog_image.return_value = [dict(name="Affenpinscher"), dict(name="Appenzeller Sennenhund")]
 
+        # Set up the environment variable for image paths
+        project_directory = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        os.environ["DOG_IMAGE_PATH"] = os.path.join(project_directory, "static", "img")
+
         result = get_matched_breeds(quiz_input, n=2)
         expected = {
             "Affenpinscher": "static/img/Affenpinscher.jpg",
             "Appenzeller Sennenhund": "static/img/Appenzeller Sennenhund.jpg"
         }
+
         self.assertEqual(result, expected)
+
+        # Clean up the environment variable after the test
+        del os.environ["DOG_IMAGE_PATH"]
 
     @patch("utils.download_dog_image")
     def test_perfect_matches_edge_case1(self, mock_download_dog_image):
@@ -26,12 +35,17 @@ class TestMatchedBreeds(TestCase):
         mock_download_dog_image.return_value.status_code = 200
         mock_download_dog_image.return_value = [dict(name="Afghan Hound"), dict(name="Bergamasco Sheepdog")]
 
+        project_directory = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        os.environ["DOG_IMAGE_PATH"] = os.path.join(project_directory, "static", "img")
+
         result = get_matched_breeds(quiz_input, n=2)
         expected = {
             "Afghan Hound": "static/img/Afghan Hound.jpg",
             "Bergamasco Sheepdog": "static/img/Bergamasco Sheepdog.jpg"
         }
         self.assertEqual(result, expected)
+
+        del os.environ["DOG_IMAGE_PATH"]
 
     @patch("utils.download_dog_image")
     def test_perfect_matches_edge_case2(self, mock_download_dog_image):
@@ -41,12 +55,17 @@ class TestMatchedBreeds(TestCase):
         mock_download_dog_image.return_value.status_code = 200
         mock_download_dog_image.return_value = [dict(name="Barbado da Terceira"), dict(name="Doberman Pinscher")]
 
+        project_directory = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        os.environ["DOG_IMAGE_PATH"] = os.path.join(project_directory, "static", "img")
+
         result = get_matched_breeds(quiz_input, n=2)
         expected = {
             "Barbado da Terceira": "static/img/Barbado da Terceira.jpg",
             "Doberman Pinscher": "static/img/Doberman Pinscher.jpg"
         }
         self.assertEqual(result, expected)
+
+        del os.environ["DOG_IMAGE_PATH"]
 
     def test_no_matches(self):
 
